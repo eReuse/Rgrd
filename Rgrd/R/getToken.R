ed56ef5a0def60504e4d0b85089855ea4ebb327b
David Franquesa <d.franquesa@ereuse.org>
2015-06-08 22:58:34 +0200

    Documennt functions

--- Rgrd/R/getToken.R
+++ Rgrd/R/getToken.R
@@ -1,14 +1,15 @@
 
-#' Get GRD Token
+#' Get Token
 #' 
-#' \code{getToken} uses getToken to get a token for GRD
-#' @param url an uri, for example http://sandbox.ereuse.org/api-token-auth/
-#' @param username an string with the username of a user
-#' @param password an string
-#' @usage urlToken  <- "http://sandbox.ereuse.org/api-token-auth/"
-#' @usage token     <- getToken(urlToken, username = "xxxx", password = "xxxx")
-#' @return a string qith token id
-
+#' \code This function is used to get authentication token to access to your grd session. This will enable many bits of functionality as well as allow other commands to provide more options
+#' @usage getToken(urlToken, username, password)
+#' @param url an uri pointing to a RESTful API to provide a token access, http://sandbox.ereuse.org/api-token-auth/
+#' @param The username (string) provided by ereuse.org federated platforms.
+#' @param The password (string) provided by ereuse.org federated platforms.
+#' @details 
+#' @return a string with a token id
+#' @seealso http://sandbox.ereuse.org/swagger-ui/#!/default/post_api_token_auth
+#' @examples 
 
 getToken <- function(url,username,password){
   result    <- postForm(uri = url, username=username, password = password,.checkParams = TRUE, style = 'HTTPPOST')
