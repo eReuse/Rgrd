6bca909d5566c3e7b10fc7e950dbc43d7555ad07
David Franquesa <d.franquesa@ereuse.org>
2015-06-09 03:19:38 +0200

    Manual, thrown exceptions, set documentation

--- Rgrd/R/getToken.R
+++ Rgrd/R/getToken.R
@@ -2,19 +2,30 @@
 #' Get Token
 #' 
 #' \code{getToken} This function is used to get authentication token to access to your grd session. This will enable many bits of functionality as well as allow other commands to provide more options
-#' @usage getToken(urlToken, username, password)
-#' @param url an uri pointing to a RESTful API to provide a token access, http://sandbox.ereuse.org/api-token-auth/
-#' @param The username (string) provided by ereuse.org federated platforms.
-#' @param The password (string) provided by ereuse.org federated platforms.
+#' @usage getToken(url = "xxx", username = "xxx", password = "xxx")
+#' @param url an uri pointing to a RESTful API to provide a token access
+#' @param username (string) provided by ereuse.org federated platforms.
+#' @param password (string) provided by ereuse.org federated platforms.
 #' @details If username or password is invalid HTTP Status Code = 400
-#' @return a string with a token id
+#' @return a list (result, error, call())
+#' @return result contains token id#' 
 #' @seealso http://sandbox.ereuse.org/swagger-ui/#!/default/post_api_token_auth
-#' @examples 
-#' urlToken  <- "http://sandbox.ereuse.org/api-token-auth/"
-#' token     <- getToken(urlToken, username = "xxx", password = "xxx")
 
 getToken <- function(url,username,password){
-  result    <- postForm(uri = url, username=username, password = password,.checkParams = TRUE, style = 'HTTPPOST')
-  data      <- rjson::fromJSON  (result, method = "C", unexpected.escape = "skip")
-  return(data$token)  
+  out <- tryCatch(
+    { 
+      if(!is.null(url)&!is.null(username)&!(is.null(password))){
+        result <- postForm(uri = url, username=username, password = password,.checkParams = TRUE, style = 'HTTPPOST')
+        data   <- rjson::fromJSON  (result, method = "C", unexpected.escape = "skip")
+        return(list(result = data$token, error=F, call = list(url = url, username = username)))        
+      } else {
+          return(list(result = simpleError(message = "Some arguments are NULL"), error=T))        
+      }              
+    },
+    error = function(cond){
+      return(list(result=cond, error=T))
+    },
+    finnally ={}
+  )
+return(out)
 }
